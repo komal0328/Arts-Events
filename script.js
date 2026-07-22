cf24e4420bf7bd08b2f78687f3f4bd43083b5063
komal bhole <kmlbhole@gmail.com>
2026-07-22 23:59:49 +0530

    header, hero section, gallery, foooter completed

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-function filtergallery(category){
-    let items=document.querySelectorAll(".gallery-item");
-    items.forEach(function(item){
-       if(category==='all'){
-        items.style.display="block";
-       }
-       else{
-        if(item.classList.contains(category)){
-            item.style.display="block";
-        }
-        else{
-            item.style.display="none";
-        }
-       }
-    });
+fetch("navbar.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("navbar").innerHTML = data;
+  });
+
+function filterGallery(category) {
+  let items = document.querySelectorAll(".gallery-item");
+  items.forEach(function (item) {
+    if (category === "all" || item.classList.contains(category)) {
+      item.style.display = "block";
+    } else {
+      item.style.display = "none";
+    }
+  });
 }
+
+fetch("footer.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("footer").innerHTML = data;
+  });
